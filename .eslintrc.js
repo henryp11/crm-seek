42b85dfca6cc1ac26e86a601b2d414990aa28e52
henryR <henry11snake@hotmail.com>
2024-06-14 15:31:48 -0500

    Se añade regla a Slint para que no de error de variables sin usar

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     "jsx-a11y/no-noninteractive-element-to-interactive-role": "off",
     "react-hooks/exhaustive-deps": "off",
     "@next/next/no-img-element": "off",
+    "no-unused-vars": "off",
   },
 };
